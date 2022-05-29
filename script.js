ec9df4c0a89839a0b3a203bf4668517885482452
Jonathan Costa <jonathancosta746@gmail.com>
2022-05-28 22:16:15 -0300

    adicionado design com nuvens e céu, javascript incrementando algumas funções

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const mario = document.querySelector('.mario');
+const tubulação = document.querySelector('.tubulação');
 
 const jump = () => {
     mario.classList.add('jump');
@@ -7,5 +8,23 @@ const jump = () => {
         mario.classList.remove('jump');
     }, 1500);
 }
+
+const loop = setInterval(() => {
+    const tubulaçãoPosition = tubulação.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+        if(tubulaçãoPosition <= 120 && tubulaçãoPosition > 0 && marioPosition < 80) {
+
+            tubulação.style.animation = 'none';
+            tubulação.style.left = '${tubulaçãoPosition}px';
+
+            mario.style.animation = 'none';
+            mario.style.left = '${marioPosition}px';
+
+            mario.src = './images/game-over.png';
+            mario.style.width = '75px';
+            mario.style.marginLeft = '50px';
+        }
+}, 10)
 
 document.addEventListener('keydown', jump);
